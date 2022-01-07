1df82374a76c8db93eda8327e87fb81e669a3bcf
Alvarito050506 <donfrutosgomez@gmail.com>
2022-01-07 20:19:10 -0300

    Use apt-get.

--- ci.sh
+++ ci.sh
@@ -4,10 +4,10 @@ set -e
 
 export DEBIAN_FRONTEND="noninteractive"
 
-apt update
-apt upgrade -y
-apt install -y git make fakeroot dpkg-dev build-essential libgtk-3-dev libjson-glib-dev valac
-apt --fix-broken install
+apt-get update
+apt-get upgrade -y --with-new-pkgs
+apt-get install -y git make fakeroot dpkg-dev build-essential libgtk-3-dev libjson-glib-dev valac
+apt-get --fix-broken install
 
 make
 make pack
